67e28303d258da78b03add9c08b917e2d6e3b195
Jigar Polra <jigarpolra@live.co.uk>
2018-07-18 16:12:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  drivers.forEach(x => console.log(x.name));
+}
+
+function logDriversByHometown(drivers, location) {
+ drivers.forEach(x => {
+   if(x.hometown === location) {
+     console.log(x.name);
+   }
+ });
+}
+
+function driversByRevenue(drivers) {
+  newObj = Object.assign([], drivers);
+  return newObj.sort((a, b) => a.revenue > b.revenue);
+}
+
+function driversByRevenue(drivers) {
+  newObj = Object.assign([], drivers);
+  return newObj.sort((a, b) => a.revenue > b.revenue);
+}
+
+function driversByName(drivers) {
+  newObj = Object.assign([], drivers);
+  return newObj.sort((a, b) => a.name.localeCompare(b.name));
+}
+
+function totalRevenue(drivers) {
+  return drivers.reduce( ((count, value) => count += value.revenue), 0);
+}
+
+function averageRevenue(drivers) {
+  return totalRevenue(drivers) / drivers.length;
+}
